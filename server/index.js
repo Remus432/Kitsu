5f598cdd58f2481b20f2cdf8099bb02707f145e1
Toporasca Bardita <romaniaromania90@yahoo.com>
2018-02-20 00:03:03 +0200

    committed

---
+++ server/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var path = require('path');
+var port = process.env.PORT || 3000
+
+var app = express();
+
+// Middleware
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.use('/public', express.static('public'));
+
+// Routes
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.listen(port, function() {
+  console.log('Running on port 1337');
+});
